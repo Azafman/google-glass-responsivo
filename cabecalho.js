91f875fdf473f49bf468dd24587b310bc3f9ef5f
Azafman <105893924+Azafman@users.noreply.github.com>
2022-09-20 16:42:30 -0300

    Empurrando o projeto em fase de andamento

---
+++ cabecalho.js
@@ -0,0 +1,22 @@
+function mudaClasse(objeto) {
+     // objeto.classList.toggle('item-muda');
+     const items = objeto.getElementsByClassName('item-menu')
+     const navegacao = document.getElementsByClassName('nav')
+     setTimeout(() => {
+          navegacao[0].classList.toggle('nav1');
+     }, 400)
+     for(let i in items) {
+          items[i].classList.toggle('item-menu1');
+     }
+}                                                                
+(function() {
+     const menu = document.getElementsByClassName('container-menu')[0];
+     const iframe = document.getElementsByTagName('iframe')[0]
+     
+     let largura = window.screen.width;
+     if(largura >= 850) {
+          menu.style.display = 'none';
+          //iframe.classList.toggle('nav1');
+          console.log(iframe);
+     }    
+})();
