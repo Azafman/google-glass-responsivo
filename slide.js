bb804de430ade16f75099ce50f48e86e65a2f3c6
Azafman <0000880867@senaimgaluno.com.br>
2022-09-26 09:43:24 -0300

    Changes of fotos.html

--- slide.js
+++ slide.js
@@ -1,3 +1,6 @@
+function iniciar() {
+     sizeOfThings();
+}
 function mostrarLegenda(span) {
      const spanLeg = document.styleSheets[1].cssRules[3]
      if(spanLeg.style.opacity != 1) {
@@ -5,34 +8,40 @@ function mostrarLegenda(span) {
      } else {
           spanLeg.style.opacity = 0
      }
-     
 }
-function sizeOfThings() {
+ function sizeOfThings() {
      let larguraDispositivo = window.innerWidth;
-     const imgs = document.querySelectorAll('span.slide-img');
-
+     
      if(larguraDispositivo < 450 ) {
-          setTimeout(changeSpan(imgs), 4000);
+          window.setTimeout(changeSpan, 5000);
      } else if (larguraDispositivo < 620 ) {
-
+          
      } else if (larguraDispositivo < 700 ) {
 
      } else if (larguraDispositivo < 755 ) {
-
+          
      } else if (larguraDispositivo < 900 ) {
 
      } else {
 
      }
+     sizeOfThings();
 }
 
-function changeSpan(imagens) {
-     console.log(imagens)
+function changeSpa() {
+     const imagens = document.querySelectorAll('span.slide-img');
 
      imagens.forEach((el,index, array) => {
-          if(index === 0) {
-               el.classList.replace(el.classList[1],imagens[index+1].classList[1]);
+          el.classList.add(imagens[index+1].classList[1]); 
+
+
+          if(index === imagens.length-1) {
+               el.classList.replace(imagens[index+1].classList[1])
           }
+          if(index === 0) {
+               el.classList.add('muda1');
+          }
+               // el.classList.replace(el.classList[1],imagens[index+1].classList[1]);
      });
-     // sizeOfThings();
+     sizeOfThings();
 }
