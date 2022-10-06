b0912efb68cbe9dea45cce53a663f51b12ab290f
Azafman <0000880867@senaimgaluno.com.br>
2022-10-06 11:23:40 -0300

    AA2

--- specs.js
+++ specs.js
@@ -1,7 +1,5 @@
-(function(){
-     let larguraSpecs = window.screen.width;
-     if(larguraSpecs >= 800) {
-          const iframeCabecalho = document.getElementsByClassName('img-mao');
-          iframeCabecalho[0].classList.toggle('vira-de-novo')
+(function() {
+     function naveguePara() {
+          
      }
 })();
