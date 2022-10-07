de1a5e952b311c521fef2c4790224f33db4d25c8
Azafman <0000880867@senaimgaluno.com.br>
2022-10-07 10:57:36 -0300

    AA5

--- specs.js
+++ specs.js
@@ -3,17 +3,8 @@ function naveguePara(cabecalho, main, localMap) {
      cabecalho.classList.add('order2')
      main.children[0].style.marginTop = 0;
 
-     document.querySelector(`section.${localMap}`).classList.add('order1');
-     for(let c in main.children) {
-          main.children[c].classList.add('order2')
+     for(let c = 0; c < main.children.length; c++) {
+          main.children[c].classList.remove('order1')
      }
-//      if(localMap == 'camera') {
-//           main.children[1].classList.add('order1');
-//      } else if (localMap == 'bateria') {
-//           main.children[3].classList.add('order1');
-//      } else if (localMap == 'sensores') {
-//           main.children[2].classList.add('order1');
-//      } else if (localMap == 'tela') {
-//           main.children[0].classList.add('order1');
-//      }
+     document.querySelector(`section.${localMap}`).classList.add('order1');
 }
