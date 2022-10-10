de5d9907009115e3e7c935a6cea00c6d1cbb4071
Azafman <105893924+Azafman@users.noreply.github.com>
2022-10-10 20:38:34 -0300

    AA6

--- specs.js
+++ specs.js
@@ -1,4 +1,3 @@
-
 function naveguePara(cabecalho, main, localMap) {
      cabecalho.classList.add('order2')
      main.children[0].style.marginTop = 0;
@@ -7,4 +6,50 @@ function naveguePara(cabecalho, main, localMap) {
           main.children[c].classList.remove('order1')
      }
      document.querySelector(`section.${localMap}`).classList.add('order1');
+     // console.log(document.getElementsByTagName('map')[0].children[0].getAttribute('coords'))
+     determineCoords(document.getElementsByTagName('map')[0].children, screen.width);
+}
+function determineCoords(mapas, largura) {
+     if(largura > 1150) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura > 1050 && largura < 1150) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura > 950 && largura < 1050) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura > 850 && largura < 950) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura <= 850) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura <= 700 && largura > 600) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+          
+     } else if(largura <= 600) {
+          mapas[0].getAttribute('coords')
+          mapas[1].getAttribute('coords')
+          mapas[2].getAttribute('coords')
+          mapas[3].getAttribute('coords')
+     }
 }
