519eeb3f0a216951414b5b47dcfa2e82cc4f0302
Azafman <105893924+Azafman@users.noreply.github.com>
2022-10-11 17:51:37 -0300

    AA8

--- specs.js
+++ specs.js
@@ -6,52 +6,44 @@ function naveguePara(cabecalho, main, localMap) {
           main.children[c].classList.remove('order1')
      }
      document.querySelector(`section.${localMap}`).classList.add('order1');
-     // console.log(document.getElementsByTagName('map')[0].children[0].getAttribute('coords'))
 }
 function determineCoords(mapas, largura) {
      if(largura > 1150) {
-          mapas[0].setAttribute('coords',)
-          // mapas[1].setAttribute('coords',)
-          // mapas[2].setAttribute('coords',)
-          // mapas[3].setAttribute('coords',)
-          // console.log(mapas[0].getAttribute('coords'))
+          mapas[0].setAttribute('coords','298,216,198,294')
+          mapas[1].setAttribute('coords','173,268,18')
+          mapas[2].setAttribute('coords','83,57,18')
+          mapas[3].setAttribute('coords','29,145,103,237,91,289,24,18')
+     } else if(largura > 850 && largura <= 1150) {
+          mapas[0].setAttribute('coords','244,174,163,236')
+          mapas[1].setAttribute('coords','142,220,18')
+          mapas[2].setAttribute('coords','71,48,18')
+          mapas[3].setAttribute('coords','25,117,85,191,78,239,16,151')
           
-     } else if(largura > 1050 && largura < 1150) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
+     } else if(largura > 700 && largura < 850) {
+          mapas[0].setAttribute('coords','284,207,187,269')
+          mapas[1].setAttribute('coords','163,253,21')
+          mapas[2].setAttribute('coords','82,53,21')
+          mapas[3].setAttribute('coords','13,176,88,269,99,222,31,145')
           
-     } else if(largura > 950 && largura < 1050) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
+     } else if(largura > 600 && largura < 700) {
+          mapas[3].setAttribute('coords','83,252,83,198,21,126,11,158')
+          mapas[1].setAttribute('coords','150,235,17')
+          mapas[2].setAttribute('coords','72,51,17')
+          mapas[0].setAttribute('coords','257,189,171,247')
           
-     } else if(largura > 850 && largura < 950) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
+     } else if(largura > 450 && largura <= 600) {
+          mapas[3].setAttribute('coords','19,93,60,143,62,185,9,117')
+          mapas[1].setAttribute('coords','109,172,12')
+          mapas[2].setAttribute('coords','54,37,14')
+          mapas[0].setAttribute('coords','123,189,191,137')
           
-     } else if(largura <= 850) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
-          
-     } else if(largura > 600 && largura <= 700) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
-          
-     } else if(largura <= 600) {
-          mapas[0].getAttribute('coords')
-          mapas[1].getAttribute('coords')
-          mapas[2].getAttribute('coords')
-          mapas[3].getAttribute('coords')
-     }
+     } else {
+          // corrigir
+          mapas[3].setAttribute('coords', '52,114,16,68,1,95,47,150')
+          mapas[1].setAttribute('coords','84,133,13')
+          mapas[2].setAttribute('coords','42,29,17')
+          mapas[0].setAttribute('coords','97,148,150,102')
+     }        
 }
 (function(){
      determineCoords(document.getElementsByTagName('map')[0].children, screen.width);
