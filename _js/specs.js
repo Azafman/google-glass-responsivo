d15ddfd3e35892f47741a15ad6e8cd68744f8f60
Azafman <105893924+Azafman@users.noreply.github.com>
2022-10-17 23:05:10 -0300

    Recovery

--- _js/specs.js
+++ _js/specs.js
@@ -8,7 +8,7 @@ function naveguePara(cabecalho, main, localMap) {
      document.querySelector(`section.${localMap}`).classList.add('order1');
 }
 function determineCoords(mapas, largura) {
-     
+     console.log(largura);
      if(largura > 1150) {
           mapas[0].setAttribute('coords','298,216,198,294')
           mapas[1].setAttribute('coords','173,268,18')
@@ -39,12 +39,14 @@ function determineCoords(mapas, largura) {
           mapas[0].setAttribute('coords','123,189,191,137')
           
      } else {
-          mapas[3].setAttribute('coords', '12,64,52,113,47,153,5,96')
+          mapas[3].setAttribute('coords','12,64,52,113,47,153,5,96')
           mapas[1].setAttribute('coords','84,133,13')
           mapas[2].setAttribute('coords','42,29,17')
           mapas[0].setAttribute('coords','97,148,150,102')
      }        
 }
-(function(){
-     determineCoords(document.getElementsByTagName('map')[0].children, screen.width);
-})(); 
+
+(function() {
+     determineCoords(document.getElementsByTagName('map')[0].children, screen.width)
+})();
+
