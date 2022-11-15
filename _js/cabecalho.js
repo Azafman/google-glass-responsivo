b4e4b1d71fef3a779ef13377e74b1eb7460ee665
Azafman <105893924+Azafman@users.noreply.github.com>
2022-11-15 11:46:01 -0300

    nova funcionalidade mouseenter -> .link

--- _js/cabecalho.js
+++ _js/cabecalho.js
@@ -1,36 +1,36 @@
 window.addEventListener('load', start);
-function chmScroll() {
-     console.log('scroll');
-}
+
 function appearItem(itens) {
      itens.forEach(el => {
           el.classList.replace('scroll', 'event-start');
      });
 }
-
 function toggleClass(itens) {
      itens[0].parentElement.classList.toggle("nav1");
      itens.forEach(el => el.classList.toggle('item-menu1'));
      document.querySelector('.nav').classList.toggle("nav1");
 }
-
-document.addEventListener('scroll', chmScroll)
 document.addEventListener('click', (e) => {
      if(e.target.classList.contains('container-menu') || e.target.classList.contains('container-menu')) {
           toggleClass(document.querySelectorAll(".item-menu"));
      }
 
 })
-
 function start() {
-     appearItem(document.querySelectorAll(".scroll"));//essa função será responsável pelo efeito de scroll
-}
-
-
-// function calcularCompra(qtde = 0) {
-//      if(document.querySelector('#resposta-pedido').checked) {
-//           document.querySelector('input#tot-Glass').value = `R$ ${(qtde * 549.99).toFixed(2)}`
-//      } else {
+     appearItem(document.querySelectorAll(".scroll"));
+     if(window.innerWidth > 850) {
+          const pagsSite = document.querySelectorAll('.link');
+          const imageMenu = document.querySelector('.img-menu');
           
-//      }
-// }                                                            
+          const imageChange = image => {
+               imageMenu.setAttribute('src',`_imagens/${image}`);
+          }
+          
+          pagsSite.forEach( el => {
+               el.addEventListener('mouseenter', e => {
+                    const imageToChange = e.target.getAttribute('href').replace('html','png');
+                    imageChange(imageToChange);
+               })
+          })
+     } 
+}
